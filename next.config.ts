b7acb2abd6433ce5da7d3f4b648356c46b50e52f
Megavannan Kandasamy <megavannan@Megavannans-MacBook-Air.local>
2026-02-18 00:29:34 +0530

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/Aethel-Cipher" : "",
   output: "export",  // Required for static site generation
   images: {
     unoptimized: true, // Required as GitHub Pages doesn't support Next.js Image Optimization
