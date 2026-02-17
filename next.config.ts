1061edb209a450e56161b6d633beef396b7ec83b
Megavannan Kandasamy <megavannan@Megavannans-MacBook-Air.local>
2026-02-18 00:29:14 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // Required for static site generation
+  images: {
+    unoptimized: true, // Required as GitHub Pages doesn't support Next.js Image Optimization
+  },
 };
 
 export default nextConfig;
